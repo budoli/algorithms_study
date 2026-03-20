d02b1ab9dbba9e387b40efe03aebe8facef2d11b
budoli <als01540@gmail.com>
2026-03-21 02:17:39 +0900

    [Bronze I] Title: 평균, Time: 204 ms, Memory: 18828 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
-        int A[] = new int[N];
-        for(int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+
+        int[] array = new int[N];
+
+        int max = array[0];
+        int sum = 0;
+
+        for (int i = 0; i< array.length; i++) {
+            array[i] = sc.nextInt();
+
+            if (max <= array[i]) {
+                max = array[i];
+            }
+            sum += array[i];
+
         }
-        long max = 0;
-        long sum = 0;
-        for(int i = 0; i < N; i++) {
-            if(A[i] > max) max = A[i];
-            sum = sum + A[i];
-        }
-        System.out.println(sum * 100.0 /max/N);
+
+        double avg = (double)sum * 100 / max / N;
+
+        System.out.println(avg);
     }
 }
