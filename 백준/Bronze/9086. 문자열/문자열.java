fc73b897ed09c8f2826f946683e300dae4dae4ee
budoli <als01540@gmail.com>
2026-03-22 00:35:44 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = sc.nextInt();
-        for (int i = 1; i <= T; i++) {
-            String S = sc.next();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
 
-            System.out.println(S.charAt(0) + "" + S.charAt(S.length()-1));
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            String word = st.nextToken();
+
+            System.out.println(word.charAt(0) + "" + word.charAt(word.length()-1));
         }
     }
 }
